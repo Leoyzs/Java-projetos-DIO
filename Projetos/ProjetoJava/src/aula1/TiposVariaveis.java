c66ba5dcdedb2efd3279c1b7b66e6681b77e09bd
Leoyz <leonardovieiravila2004@gmail.com>
2024-05-11 19:12:01 -0300

    commit dos operadores

--- Projetos/ProjetoJava/src/aula1/TiposVariaveis.java
+++ Projetos/ProjetoJava/src/aula1/TiposVariaveis.java
@@ -3,13 +3,14 @@ package aula1;
 public class TiposVariaveis {
 	 public static void main (String [] args){
 		 
+		/* 
 		 byte Idade = 87;
 		 short ano = 2024;
 		 int cep = 83302250;
 		 long cpf = 1287267190;
 		 float pi = 3.14F;
 		 double salario = 1225.75;
-		 System.out.println(pi);
+		 System.out.println("O NUMERO pi É" +pi);
 		 
 		 short numeroCurto = 1;
 		 int numeroNormal = numeroCurto;
@@ -18,6 +19,29 @@ public class TiposVariaveis {
 
 		 final int numerofixo = 1;
 		 System.out.println(numerofixo);
+		 */
+		 
+		 int a = 5;
+		 int b = 5;
+		 
+		 /* MODULO IF ELSE
+		 
+		 String resultado;
+		 
+		 if(a==b){
+			  resultado = "A é igual a B";
+			 System.out.println(resultado);
+		 }
+		 
+		 else{
+			  resultado = "A não é igual a B";
+			 System.out.println(resultado);
+		 }	 */
+		 
+		 
+		 // TESTE AULA 3 IF E ELSE DE UMA MANEIRA RESUMIDA
 
+		 String resultado = a==b ? "Verdadeiro" : "falso";
+		 System.out.println(resultado);
 	 }
 }
