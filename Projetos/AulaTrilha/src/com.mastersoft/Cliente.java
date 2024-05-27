c5728f6906f610917533f34103daa4c326eeba0c
Leoyz <leonardovieiravila2004@gmail.com>
2024-05-27 10:20:44 -0300

    Salvando POO

---
+++ Projetos/AulaTrilha/src/com.mastersoft/Cliente.java
@@ -0,0 +1,10 @@
+
+public class Cliente {
+public static void main(String[] args) {
+    Exame exame = new Exame();
+    int i=0;
+    System.out.println(exame.tiposExames[i]);
+}    
+
+
+}
---
+++ Projetos/AulaTrilha/src/com.mastersoft/Exame.java
@@ -0,0 +1,5 @@
+public class Exame {
+ String tiposExames[] = {"odontologico", "oftamologistico"};   
+
+
+}
