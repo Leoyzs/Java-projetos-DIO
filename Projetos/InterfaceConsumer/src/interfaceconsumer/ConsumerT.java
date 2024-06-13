022fa77ec42db2ba5afbc02fbc6ab590933f5970
Leoyz <leonardovieiravila2004@gmail.com>
2024-06-13 20:56:53 -0300

    Salvando ConsumerT

---
+++ Projetos/InterfaceConsumer/src/interfaceconsumer/ConsumerT.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaceconsumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author leo
+ */
+public class ConsumerT {
+    public static void main (String [] args){
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        numeros.forEach(n -> { {
+                if( n % 2 == 0){
+                    System.out.println(n);
+                }
+            }
+        });
+        
+    }
+}
